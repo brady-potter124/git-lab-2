d8fcbf1489dde37887fb464ec7ec21b0aa40c5d2
Brady Potter <bp354719@ohio.edu>
2021-01-29 15:34:33 -0500

    Added git-lab-program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Brady Potter>
+ *        Date: <January 29 2021>
+ * Description: Lab 2 final steps
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello, Git!" << endl;
+    return 0;
+}// main
